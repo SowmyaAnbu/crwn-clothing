d7b8d48ec0863e4c7bdeab48cf76687ab58f5b2c
SowmyaAnbu <s.sowmeya@gmail.com>
2026-02-28 11:39:59 +0000

    adding redirects for netlify

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -13,8 +13,6 @@ const getButton = (buttonType =  BUTTON_TYPE_CLASSES.base) => ({
 
 const Button = ({children, buttonType, ...otherProps}) => {
     const CustomButton = getButton(buttonType);
-     console.log('buttonType:', buttonType);
-    console.log('CustomButton:', CustomButton);
     return <CustomButton {...otherProps}>{children}</CustomButton>
     
 }
