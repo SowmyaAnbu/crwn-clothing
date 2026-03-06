e4d2b55e02caa07b1317c4355aa5b26a1d740a84
SowmyaAnbu <s.sowmeya@gmail.com>
2026-03-06 17:57:56 +0000

    added Redux thunk middleware to handle async logic

--- src/store/categories/category-selector.js
+++ src/store/categories/category-selector.js
@@ -15,5 +15,10 @@ export const selectCategoriesMap = createSelector(
     }, {})
 )
 
+export const selectCategoriesIsLoading = createSelector(
+    [selectCategoryReducer],
+    (categoriesSlice) => categoriesSlice.isLoading
+)
+
 
 
--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,7 @@ import {compose, createStore, applyMiddleware} from 'redux'
 import {logger} from 'redux-logger'
 import { persistStore, persistReducer } from "redux-persist";
 import storage from 'redux-persist/lib/storage'
+import {thunk} from 'redux-thunk'
 import { rootReducer } from './root-reducer'
 
 const persistConfig = {
@@ -12,7 +13,7 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middlewares = [process.env.NODE_ENV !== 'production' && logger].filter(
+const middlewares = [process.env.NODE_ENV !== 'production' && logger, thunk].filter(
     Boolean
 )
 const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && 
